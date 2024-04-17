1558ee8de554562e3b71028227e0f09ac1954040
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-17 12:28:43 -0500

    up arrow fixed

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,7 +4,7 @@
     export let tabs =  [
         { name: "Projects ", link: "#projects" },
         { name: "About me", link: "#about" },
-        { name: "Blog", link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
+        // { name: "Blog", link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
     ];
  
 </script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
   (y > 0
       ? " opacity-full pointer-events-auto"
       : " pointer-events-none opacity-0")}>
-<button onclick={goTop} class="ml-auto rounded-full aspect-square bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+<button  on:click={goTop} class="ml-auto rounded-full aspect-square bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
 >
   <i class="fa-solid fa-arrow-up"></i>
 </button>
